cab8d14078e9e4ae7aaf0ea4b862608e0590de7e
meliphyra-ux <xennerit@icloud.com>
2022-10-25 00:44:29 +0300

    Another changes in styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import React, {FC} from 'react';
+import point from "../assets/point.svg"
 
 export interface CardProps{
     _id?: string;
@@ -20,14 +21,21 @@ const Card:FC<CardProps> = ({
     price
 }) => {
     return (
-        <figure className="w-1/4 bg-[#161617] rounded-xl">
+        <figure className="w-1/4 text-light bg-[#161617] rounded-xl">
             <img src={imgSrc} alt="View of apartments" className="object-fit w-full aspect-square object-center"/>
-            <h3>📍{location}</h3>
-            <h5>{beds} beds</h5>
-            <h5>{area} m</h5>
-            <h5>{yearOfBuilding} s</h5>
-            <button>Book Now</button>
-            <h2>${price}</h2>
+            <article className="px-[26px] pb-6">
+                <div className="flex flex-row items-center">
+                    <img className="mr-2" src={point} alt=""/>
+                    <h3 className="text-[#D0D0D1]">{location}</h3>
+                </div>
+                <div className="flex justify-between">
+                    <h5 className="text-[#969696]">{beds} beds</h5>
+                    <h5 className="text-[#969696]">{area} m</h5>
+                    <h5 className="text-[#969696]">{yearOfBuilding} s</h5>
+                </div>
+                <button>Book Now</button>
+                <h2 className="inline-block w-2/3  text-end">${price}</h2>
+            </article>
         </figure>
     );
 };
--- src/pages/main/PopularResidence.tsx
+++ src/pages/main/PopularResidence.tsx
@@ -21,7 +21,7 @@ const PopularResidence: FunctionComponent = () => {
             <h2 className="text-5xl font-[Sora, sans_serif] font-bold inline-block w-full relative">Our Popular
                 Residence <Link to="#" className="absolute top-1/2 -translate-y-1/2 right-0 text-lg font-light">Explore
                     All {"-->"}</Link></h2>
-            <div className="flex flex-row">
+            <div className="flex flex-row justify-between">
                 {flats && flats.map(flat => (
                     <Card key={flat._id} imgSrc={flat.imgSrc} location={flat.location} beds={flat.beds} area={flat.area}
                           yearOfBuilding={flat.yearOfBuilding} price={flat.price}/>
